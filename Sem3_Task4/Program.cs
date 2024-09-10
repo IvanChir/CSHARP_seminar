d437a55fb8c6e9d82d98bd33a6068345c26864b9
IvanChi <ichirkov95@yandex.ru>
2024-09-10 15:06:44 +0400

    Addet Sem3_Task4

--- Sem3_Task4/Program.cs
+++ Sem3_Task4/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4
+// Дано натуральное трёхзначное число. Создайте массив, состоящий из цифр этого числа. Младший разряд
+// числа должен располагаться на 0- м индексе массива, старший – на 2-м.
+// Пример
+// 456 => [6 5 4]
+// 781 => [1 8 7]
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int[] array = new int[3]; //создали новый массив из 3 переменных
+    int index = 0; // последний индекс массива 
+    while (num != 0) // цикл while для разбиеня трехзначного числа, последующим разворачиванием
+    {
+        array[index] = num % 10;
+        num /= 10;
+        index++;
+    }
+
+    // сборка числа как массив
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++) 
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+else
+{
+    Console.Write("Некорректный ввод!");
+}
