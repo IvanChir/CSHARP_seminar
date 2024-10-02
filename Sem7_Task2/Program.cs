08118751206c5ab83ec9f82b63064258b383f5fb
IvanChi <ichirkov95@yandex.ru>
2024-10-02 19:01:01 +0400

    Addet seminar 7

---
+++ Sem7_Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая будет принимать на
+// вход число и возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+
+// шаблон для решения взят из факториала:
+
+// int Factorial(int n)
+// {
+//     if (n == 1 || n == 0) return 1;
+//     return n * Factorial(n - 1);
+// }
+
+// Console.WriteLine(Factorial(5)); // 120
+
+
+int SumDigits(int num)  //123  //12 //1  //0
+{
+    if (num == 0) return 0;  //
+    return num % 10 + SumDigits(num / 10); // 123 //12 //1 
+    // 1 % 10 + 12 % 10 + 123 % 10 = 1 + 2 + 3 = 6 + 0  
+}
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigits(number));
+
+
+
+
+
---
+++ Sem7_Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Считать строку с консоли, содержащую латинские
+// буквы. Вывести на экран согласные буквы этой
+// строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+// создаем метод через рекурсию 
+
+void PrintConsonans(string str, int index = 0)
+{
+    if (index == str.Length)
+    {
+        return;
+    }
+    CheckPrintConsonans(str, index);
+
+    PrintConsonans(str, index + 1);
+}
+
+// метод для проверки на отсутствие гласных букв
+void CheckPrintConsonans(string str, int index)
+{
+    string vowels = "aeiouy";
+
+    if (char.IsAsciiLetter(str[index]) & !vowels.Contains(str[index])) // если вводимая строка является буквой и не содержит гласные буквы
+    {
+        Console.Write($"{str[index]} ");
+    }
+}
+
+Console.WriteLine("Введите строку содержащую латинские буквы: ");
+string text = Console.ReadLine();
+
+PrintConsonans(text);
+
